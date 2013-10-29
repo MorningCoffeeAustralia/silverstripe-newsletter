3f1b82fc71cd721f6e167a5d32992dda3f251e0b
Alexis Hope <bambishope@gmail.com>
2013-10-30 09:06:58 +1100

    add article to draft newsletter
    don't know why its marking these files as new. I stashed then stash popped the changes onto master

---
+++ code/dataobjects/NewsletterArticle.php
@@ -0,0 +1,40 @@
+<?php
+class NewsletterArticle extends DataObject {
+	public static $db = array(
+		'Title' => 'Varchar(100)',
+		'Body' => 'Text'
+	);
+
+	public static $has_one = array (
+		'Image' => 'BetterImage',
+		'Newsletter' => 'Newsletter'
+	);
+
+	public function getNewsletterArticleEditForm() {
+		$fields = $this->getCMSFields();
+//		$test = $this->exists();
+//		if( $this->exists() != 1 ) {
+////			$fields->removeFieldFromTab('Root.Main', 'Image');
+//			$fields->removeByName('Image');
+//		}
+		$fields->push($field = new HiddenField("ID"));
+		$field->setValue($this->ID);
+
+		$actions = $this->getCMSActions();
+
+		$form = new Form($this, "NewsletterArticleEditForm", $fields, $actions);
+		$form->loadDataFrom($this);
+
+		$newsletter = $this->Newsletter();
+		if($newsletter->Status != 'Draft') {
+			$readonlyFields = $form->Fields()->makeReadonly();
+			$form->setFields($readonlyFields);
+		}
+
+		return $form;
+	}
+	
+	public function link() {
+		return '$this->Image()->Link()';
+	}
+}
