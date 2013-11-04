acdb54d3d9a601325759a3b2a77429a8202195ef
Adam Rice <development@hashnotadam.com>
2013-11-04 15:32:18 +1100

    Remove Newsletter HasOne field from Newsletter Article admin

--- code/dataobjects/NewsletterArticle.php
+++ code/dataobjects/NewsletterArticle.php
@@ -13,9 +13,16 @@ class NewsletterArticle extends DataObject {
 	public static $defaults = array(
 		'Title' => 'New Article'
 	);
-	
+
+	public function getCMSFields($params = null) {
+		$fields = parent::getCMSFields($params);
+
+		$fields->removeByName('Newsletter');
+
+		return $fields;
+	}
+
 	public function getLabel() {
 		return $this->Title ? $this->Title : "(Article)";
 	}
-	
 }
