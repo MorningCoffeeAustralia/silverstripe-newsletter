885bfa05406f18adf9212577909586a04f7c4bf8
Adam Rice <development@hashnotadam.com>
2013-12-19 10:38:07 +1100

    The NewsletterAdmin does not easily allow the NewsletterID to be passed to x_many relationship fields so 2 new formfields have been created to make the connections

---
+++ code/formfield/NewsletterAdminManyManyDataObjectManager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The NewsletterAdmin module's handling of request parameter has been designed
+ * in such a way that it works poorly with the ComplexTableField and its children.
+ * Features such as search and only_related do not have a link to the
+ * Newsletter so they do not work.
+ */
+class NewsletterAdminManyManyDataObjectManager extends ManyManyDataObjectManager {
+	// Add the Newsletter ID to the wrapping div
+	// This is used by the search functionality
+	public function CurrentLink() {
+		return parent::CurrentLink() . $this->newsletterIDQueryArgument();
+	}
+
+	// Add the Newsletter ID to links
+	// This is used by only_related
+	public function RelativeLink($params = array()) {
+		return parent::RelativeLink($params) . $this->newsletterIDQueryArgument();
+	}
+
+	public function newsletterIDQueryArgument() {
+		return '&NewsletterID=' . $this->controller->ID;
+	}
+}
---
+++ code/formfield/NewsletterMemberTableField.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The NewsletterAdmin module's handling of request parameter has been designed
+ * in such a way that it works poorly with the ComplexTableField and its children.
+ * Features such as search do not have a link to the Newsletter so they do not work.
+ */
+class NewsletterMemberTableField extends MemberTableField {
+	// Add the Newsletter ID to the wrapping div
+	// This is used by the search functionality
+	public function SearchForm() {
+		$this->controller->setNewsletterFromRequest();
+
+		$input = '<input type="hidden" name="ID" value="' . $this->controller->newsletter->ID . '" />';
+
+		return preg_replace('/<input/', "$input <input", parent::SearchForm(), 1);
+	}
+}
