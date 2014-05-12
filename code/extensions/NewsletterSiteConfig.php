f86b1b20cc4711953abd91da600f290c3ed4e03a
Adam Rice <development@hashnotadam.com>
2014-05-12 18:04:47 +1000

    extraStatics() is not used in SS3.1

--- code/extensions/NewsletterSiteConfig.php
+++ code/extensions/NewsletterSiteConfig.php
@@ -3,14 +3,10 @@
  * @package  newsletter
  */
 
-class NewsletterSiteConfig extends DataExtension{
-	function extraStatics($class = null, $extension = null) {
-		return array(
-			'db' => array(
-				"GlobalUnsubscribe" => "Boolean",
-			)
-		);
-	}
+class NewsletterSiteConfig extends DataExtension {
+	private static $db = array(
+		'GlobalUnsubscribe' => 'Boolean'
+	);
 
 	public function updateCMSFields(FieldList $fields) {
 		$fields->addFieldToTab("Root",
